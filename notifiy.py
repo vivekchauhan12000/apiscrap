97fa26ceaab500bc6f80039037d36b7721eaa7f2
vivekchauhan12000 <49834398+vivekchauhan12000@users.noreply.github.com>
2020-08-31 21:09:04 +0530

    simple_and_complete

---
+++ notifiy.py
@@ -0,0 +1,14 @@
+import notify2
+import time
+
+
+def parseFeed():
+     notify2.init('News Notify') 
+    print("it done")
+    n.set_urgency(notify2.URGENCY_NORMAL) 
+    n.show() 
+    n.set_timeout(15000) 
+    time.sleep(1200) 
+      
+if _name_ = '_main_': 
+    parseFeed() 
